bd2e1efb9427775dc3a1a24cee1a2207457a503d
Nick Lee <nicklee808@gmail.com>
2016-11-23 02:30:10 -1000

    unlocked

--- vault.js
+++ vault.js
@@ -1,12 +1,24 @@
 'use strict';
-module.exports = function() {
-  
-  function setValue(key, value){
 
+module.exports = function(){
+  
+  var lock = {};
+
+  function setValue(key,value){
+    lock[key] = value;
+    return lock;
   }
 
   function getValue(key){
-
+    if(lock[key]){
+      return lock[key];
+    } else {
+      return null;  
+    }
   }
 
+  return{
+    setValue: setValue,
+    getValue: getValue
+  };
 };
